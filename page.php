fe2bec4cae06aa1e7fd11e861bd1cc78a48cd576
choeseongwoni40-prog <choeseongwoni40@gmail.com>
2026-01-06 09:27:27 +0100

    Create page.php

---
+++ page.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Page template
+ */
+get_header(); ?>
+
+<main class="site-content">
+    <?php while (have_posts()) : the_post(); ?>
+        <article <?php post_class(); ?>>
+            <header class="entry-header">
+                <h1 class="entry-title"><?php the_title(); ?></h1>
+            </header>
+
+            <div class="entry-content">
+                <?php the_content(); ?>
+            </div>
+        </article>
+    <?php endwhile; ?>
+</main>
+
+<?php get_footer(); ?>
